4976eff6ab21e69410bcc453755847837b261b4e
Шевченко Максим <shevchenko@gogpb.ru>
2020-11-09 18:03:17 +0200

    add mongooseDB

--- index.js
+++ index.js
@@ -3,51 +3,18 @@ const app = express();
 
 const cors = require('cors')
 
+require('dotenv').config()
+
 const morgan = require('morgan');
 
 const isContains = require('./utl');
 
-require('dotenv').config()
+const Person = require('./models/person')
 const port = process.env.PORT || 3000
 
-let persons = [
-  {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
-  },
-  {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
-  },
-  {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
-  },
-  {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
-  }
-]
-
-// Промежуточное ПО - это функция, которая получает три параметра:
-// custom midleware
-// const requestLogger = (request, response, next) => {
-//   console.log('Method:', request.method)
-//   console.log('Path:  ', request.path)
-//   console.log('Body:  ', request.body)
-//   console.log('---')
-//   next()
-// }
 app.use(cors())
 
 app.use(express.json())
-
-// morgan
-// app.use(morgan('tiny'))
 
 morgan.token('body', function getId (req) {
   return JSON.stringify(req.body)
@@ -55,28 +22,35 @@ morgan.token('body', function getId (req) {
 
 app.use(morgan(':body :method :url :response-time'))
 
-// app.use(requestLogger)
-
+// GET all people
 app.get('/api/person', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
 })
 
+// search by id
 app.get('/api/people/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
+  Person.findById(req.params.id).then(person => {
+    if (person) {
+      res.json(person)
+    } else {
+      res.status(404).end()
+    }
+  })
+  .catch(error => {
+    console.log(error)
+    res.status(500).end()
+  })
 })
 
 app.get('/api/info', (req, res) => {
-  res.send(`
-    <p>Phonebook has info ${persons.length - 1}</p>
-    <p>${new Date()}</p>
-  `)
+  Person.find({}).then(persons => {
+    res.send(`
+      <p>Phonebook contains information about ${persons.length} people</p>
+      <p>${new Date()}</p>
+    `)
+  })
 })
 
 const generateId = (arr) => {
@@ -94,22 +68,22 @@ app.post('/api/person/', (request, response) => {
     })
   }
 
-if (isContains(persons, 'name', body.name)) {
-  return response.status(400).json({ error: 'name must be unique' })
-}
-if (isContains(persons, 'number', body.number)) {
-  return response.status(400).json({ error: 'number must be unique' })
-}
+  Person.find({}).then(persons => {
+    if (isContains(persons, 'name', body.name)) {
+      return response.status(400).json({ error: 'name must be unique' })
+    }
+    if (isContains(persons, 'number', body.number)) {
+      return response.status(400).json({ error: 'number must be unique' })
+    }
+    const person = new Person({
+      name: body.name,
+      number: body.number,
+    })
 
-
-  const person = {
-    name: body.name,
-    number: body.number,
-    id: generateId(persons),
-  }
-
-  persons = persons.concat(person)
-  response.json(person)
+    person.save().then(savedPerson => {
+      response.json(savedPerson)
+    })
+  })
 })
 
 app.delete('/api/people/:id', (req, res) => {
